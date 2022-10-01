2709bb47936fc4ae967899747e82eec1e3068117
Dickinson <adickinson@bsu.edu>
2022-09-30 21:36:00 -0400

    Player moves around

--- player/player.gd
+++ player/player.gd
@@ -14,10 +14,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("move_left"):
-		move_and_collide(Vector2.LEFT)
-	if Input.is_action_pressed("move_Right"):
-		move_and_collide(Vector2.RIGHT)
-	if Input.is_action_pressed("move_Up"):
-		move_and_collide(Vector2.UP)
-	if Input.is_action_pressed("move_Down"):
-		move_and_collide(Vector2.DOWN)
+		var _ignore = move_and_collide(Vector2.LEFT)
+	if Input.is_action_pressed("move_right"):
+		var _ignore = move_and_collide(Vector2.RIGHT)
+	if Input.is_action_pressed("move_up"):
+		var _ignore = move_and_collide(Vector2.UP)
+	if Input.is_action_pressed("move_down"):
+		var _ignore = move_and_collide(Vector2.DOWN)
